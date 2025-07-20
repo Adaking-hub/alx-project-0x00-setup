83ed2f9ff317e156a31533bf1d57d091f080bbbd
Adaking-hub <obi.chioma478@gmail.com>
2025-07-20 16:35:02 +0100

    Next.js Project

--- alx-project-0x00/interfaces/index.tsx
+++ alx-project-0x00/interfaces/index.tsx
@@ -1,6 +1,12 @@
-// interfaces/index.tsx
+import React from "react";
+import { ButtonProps } from "@/interfaces";
 
-export interface ButtonProps {
-  title: string;
-  styles: string;
-}
+const Button: React.FC<ButtonProps> = ({ title, styles }) => {
+  return (
+    <button className={`px-4 py-2 bg-blue-500 text-white ${styles}`}>
+      {title}
+    </button>
+  );
+};
+
+export default Button;
